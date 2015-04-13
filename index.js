dfd072f2893146b6b43e5c6de5a4f8c40653c5e6
Anderson Arboleya <anderson@anew.local>
2015-04-13 12:03:14 -0300

    refactoring whole class to expand usability

--- index.js
+++ index.js
@@ -1,87 +1,92 @@
-/**
- * Module constructor
- * @param  {Object} target Target object to extends methods and properties into
- * @return {Object}        Target after with extended methods and properties
- */
-module.exports = function(target) {
-  target = target || {};
-  for(var prop in Happens)
-    target[prop] = Happens[prop];
-  return target;
-};
+(function (global, factory) {
+  module && module.exports ? module.exports = factory() :
+  define && define.amd ? define(factory) : window.Happens = fatory();
+}(this, function () {
 
+  'use strict'
 
-
-/**
- * Class Happens
- * @type {Object}
- */
-var Happens = {
+  /**
+   * Module constructor
+   * @param  {Object} target Target object to extends methods and properties into
+   * @return {Object}        Target after with extended methods and properties
+   */
+  function Happens(target){
+    var nu = this instanceof Happens;
+    if(target){
+      if(!nu)
+        for(var prop in Happens.prototype)
+          target[prop] = Happens.prototype[prop];
+      else
+        throw new Error("You can't pass a target when instantiating with the `new` keyword");
+    }
+    else if(!nu)
+      return new Happens
+  };
 
   /**
    * Initializes event
    * @param  {String} event Event name to initialize
    * @return {Array}        Initialized event pool
    */
-  __init: function(event) {
+  Happens.prototype.__init = function(event) {
     var tmp = this.__listeners || (this.__listeners = []);
     return tmp[event] || (tmp[event] = []);
-  },
+  };
 
   /**
    * Adds listener
    * @param  {String}   event Event name
    * @param  {Function} fn    Event handler
    */
-  on: function(event, fn) {
+  Happens.prototype.on = function(event, fn) {
     validate(fn);
     this.__init(event).push(fn);
-  },
+  };
 
   /**
    * Removes listener
    * @param  {String}   event Event name
    * @param  {Function} fn    Event handler
    */
-  off: function(event, fn) {
+  Happens.prototype.off = function(event, fn) {
     var pool = this.__init(event);
     pool.splice(pool.indexOf(fn), 1);
-  },
+  };
 
   /**
    * Add listener the fires once and auto-removes itself
    * @param  {String}   event Event name
    * @param  {Function} fn    Event handler
    */
-  once: function(event, fn) {
+  Happens.prototype.once = function(event, fn) {
     validate(fn);
     var self = this, wrapper = function() {
       self.off(event, wrapper);
       fn.apply(this, arguments);
     };
     this.on(event, wrapper );
-  },
+  };
 
   /**
    * Emit some event
    * @param  {String} event Event name -- subsequent params after `event` will
    * be passed along to the event's handlers
    */
-  emit: function(event /*, arg1, arg2 */ ) {
+  Happens.prototype.emit = function(event /*, arg1, arg2 */ ) {
     var i, pool = this.__init(event).slice(0);
     for(i in pool)
       pool[i].apply(this, [].slice.call(arguments, 1));
+  };
+
+  /**
+   * Validates if a function exists and is an instanceof Function, and throws
+   * an error if needed
+   * @param  {Function} fn Function to validate
+   */
+  function validate(fn) {
+    if(!(fn && fn instanceof Function))
+      throw new Error(fn + ' is not a Function');
   }
-};
 
-
-
-/**
- * Validates if a function exists and is an instanceof Function, and throws
- * an error if needed
- * @param  {Function} fn Function to validate
- */
-function validate(fn) {
-  if(!(fn && fn instanceof Function))
-    throw new Error(fn + ' is not a Function');
-}
+  return Happens;
+}));
